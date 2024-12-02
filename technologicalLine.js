8a8a494e509478e304a344e134bf401a90effd5f
SugarHedgehog <sneginca.cat@gmail.com>
2024-12-02 16:13:33 +0300

    Разделение на класс и применение

--- technologicalLine.js
+++ technologicalLine.js
@@ -138,34 +138,8 @@ class LaborPayment extends Workshop {
     }
 }
 
-// Пример использования
-const workersPerLine = 2;//количество людей на линии
-const shiftDurationMinutes = 8 * 60; // 8 часов в минутах
-const targetOutput = 2880;
-
-const lines = [
-    new TechnologicalLine(2, 3, workersPerLine, shiftDurationMinutes),
-    new TechnologicalLine(2, 3, workersPerLine, shiftDurationMinutes),
-    new TechnologicalLine(2, 3, workersPerLine, shiftDurationMinutes),
-    new TechnologicalLine(2, 3, workersPerLine, shiftDurationMinutes)
-];
-const workshop = new Workshop(lines);
-
-// Вывода информации о выполнении плана
-workshop.checkProductionPlan(targetOutput, shiftDurationMinutes);
-// Вывода информации о выпуске
-workshop.printTotalShiftOutput(shiftDurationMinutes);
-// Вывода информации о цехе
-workshop.printAllLinesInfo();
-
-const hourlyRate = 335; // почасовая оплата в рублях
-const bonusRate = 0.17; // бонус за выполнение плана
-const extraBonusRate = 0.215; // доплата за перевыполнение плана
-const costPerUnit = 726; // себестоимость одной покрышки в рублях
-
-const laborPaymentWorkshop = new LaborPayment(lines, hourlyRate, bonusRate, extraBonusRate, costPerUnit);
-
-// Вывод информации об оплате труда
-laborPaymentWorkshop.printLaborPayment(shiftDurationMinutes, targetOutput);
-// Вывод информации о зарплате всех рабочих
-laborPaymentWorkshop.printTotalLaborPayment(shiftDurationMinutes, targetOutput);
+module.exports = {
+    TechnologicalLine,
+    Workshop,
+    LaborPayment
+};
